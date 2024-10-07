bc5d5c08731ed7e04aa154a17abbbf49ef943228
Atulp24 <apk200209@gmail.com>
2024-10-07 16:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function convertToRoman(num) {
   	const obj = {
       ['M', 1000],
-	  ['CM', 900]
+	  ['CM', 900],
       ['D', 500],
-	  ['CD', 400]
+	  ['CD', 400],
       ['C', 100],
-	  ['XC', 90]
+	  ['XC', 90],
       ['L', 50],
-	  ['XL', 40]
+	  ['XL', 40],
       ['X', 10],
-	  ['IX', 9]
+	  ['IX', 9],
       ['V', 5],
-	  ['IV', 4]
+	  ['IV', 4],
       ['I', 1]
     };
 
@@ -30,9 +30,6 @@ function convertToRoman(num) {
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
-
-
-
 
 // do not edit below this line
 module.exports = convertToRoman
