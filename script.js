530e6c95a3ad6b5594dda2155128ad5525601289
Atulp24 <apk200209@gmail.com>
2024-10-07 16:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function convertToRoman(num) {
   	const obj = {
-      ['M', 1000],
-	  ['CM', 900],
-      ['D', 500],
-	  ['CD', 400],
-      ['C', 100],
-	  ['XC', 90],
-      ['L', 50],
-	  ['XL', 40],
-      ['X', 10],
-	  ['IX', 9],
-      ['V', 5],
-	  ['IV', 4],
-      ['I', 1]
-    };
+	  'M': 1000,
+	  'CM': 900,
+	  'D': 500,
+	  'CD': 400,
+	  'C': 100,
+	  'XC': 90,
+	  'L': 50,
+	  'XL': 40,
+	  'X': 10,
+	  'IX': 9,
+	  'V': 5,
+	  'IV': 4,
+	  'I': 1
+	};
 
 	let result = "";
-    for(let i of obj) {
-		let[symbol, value] = i;
-
-		while(num>=value) {
-			result+=symbol;
-			num-=value;
-		}
+    for(let symbol in obj) {
+	    let value = obj[symbol];
+	
+	    while(num >= value) {
+	        result += symbol;
+	        num -= value;
+	    }
 	}
     return result;
 
